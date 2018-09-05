6e84bc1b7ee56292216078aa336db0bd69d12d09
Matt <Mattwall1993@gmail.com>
2018-09-05 22:40:15 +0100

    Removed search from navbar. Added Login/logout

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -15,11 +15,15 @@
             <li class="nav-item">
               <a class="nav-link" href="/register">Register</a>
             </li>
+            @else
+            <li class="nav-item">
+              <a class="nav-link" href="/logout">Logout</a>
+            </li>
             @endif
           </ul>
-          <form class="form-inline mt-2 mt-md-0">
+          {{-- <form class="form-inline mt-2 mt-md-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          </form> --}}
         </div>
       </nav>

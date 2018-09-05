3ffabee5d547f912015f8c943c9a97e883e8ce0b
Matt <Mattwall1993@gmail.com>
2018-09-05 22:41:36 +0100

    Updated posts.show to extend master

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    </head>
-    <body>
-
+@extends('layout.master')
+@section('content')
 <h1>
     {{  $post->title  }}
 </h1>
@@ -26,5 +21,4 @@
         </div>
       </div>
 
-    </body>
-</html>
+@endsection

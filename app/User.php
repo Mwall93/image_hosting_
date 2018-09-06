f6d008904e4e90fd33b7a3efd165547f2bfbd416
Matt <Mattwall1993@gmail.com>
2018-09-06 23:31:23 +0100

    Fixed typo in user.php

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function user()
+    public function Post()
     {
         return $this->hasMany(Post::class);
     }

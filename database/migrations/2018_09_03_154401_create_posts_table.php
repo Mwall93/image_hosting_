4059f0ebb16aa48cf3c88a377420adf6d092d36f
Matt <Mattwall1993@gmail.com>
2018-09-06 23:31:48 +0100

    Allowed nullable user_id in posts.
    Allows anonymous uploads

--- database/migrations/2018_09_03_154401_create_posts_table.php
+++ database/migrations/2018_09_03_154401_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('image_name');
-           // $table->integer('user_id');
+            $table->integer('user_id')->nullable();
             $table->timestamps();
         });
     }

fe95d59142c3d7bebb1919559e59601b04c473d5
Matt <Mattwall1993@gmail.com>
2018-09-10 17:57:11 +0100

    Added 'views' column to Posts table

--- database/migrations/2018_09_10_165510_add_views_to_posts.php
+++ database/migrations/2018_09_10_165510_add_views_to_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostDataTable extends Migration
+class AddViewsToPosts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreatePostDataTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_data', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('post_id');
+        Schema::table('posts', function($table) {
             $table->integer('views');
-            $table->timestamps();
         });
     }
 
@@ -28,6 +25,8 @@ class CreatePostDataTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_data');
+        Schema::table('posts', function($table) {
+            $table->dropColumn('views');
+        });
     }
 }
